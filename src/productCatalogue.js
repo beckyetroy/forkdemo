5335722046e575aca88efa113b1f4699f0abc767
Adrian Bernacki <adrianbernacki2021@gmail.com>
2022-11-08 11:14:00 +0000

    batchAdd feature developed.

--- src/productCatalogue.js
+++ src/productCatalogue.js
@@ -32,7 +32,24 @@ class Catalogue {
     result.productIds = this.products
       .filter((p) => p.quantityInStock <= p.reorderLevel)
       .map((p) => p.id);
-    return result;
+    return result
+  };
+  
+  batchAddProducts(batch) {
+    const productIDClash = batch.products.some(
+      (product) => this.findProductById(product.id) !== undefined
+    );
+    if (productIDClash) {
+      throw new Error("Bad Batch");
+    }
+    const noProductsAdded = batch.products
+      .filter((product) => product.quantityInStock > 0 )
+      .filter((p) => {
+        this.addProduct(p);
+        return true;
+      })
+      .reduce((acc, p) => acc + 1, 0);
+    return noProductsAdded;
   }
 
 }
